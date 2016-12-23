c87840b3b54387adb3bcd43ddaa3e2178228d91a
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-12-23 18:54:12 +0300

    Add Google maps integration

--- PickFriend.Web/Client/src/app/app.module.ts
+++ PickFriend.Web/Client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app.routing';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 import { AppComponent } from './app.component';
 
 import { AccountService } from './account/account.service';
@@ -14,9 +15,17 @@ import { RegisterComponent } from './account/register/register.component';
 import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
 
+const GMAPS_API_KEY = require('json!../secretconfig.json').gmapsApiKey;
+
 
 @NgModule({
-    imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule, HttpModule],
+    imports: [BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        HttpModule,
+        AgmCoreModule.forRoot({
+            apiKey: GMAPS_API_KEY
+        })],
     declarations: [AppComponent, MainComponent, LoginComponent, RegisterComponent, ProfileComponent],
     providers: [AccountService, AuthInfoStorage, AuthGuard],
     bootstrap: [AppComponent]
--- PickFriend.Web/Client/src/app/main/main.component.ts
+++ PickFriend.Web/Client/src/app/main/main.component.ts
@@ -3,11 +3,16 @@ import { Router } from '@angular/router';
 
 import { AccountService } from '../account/account.service';
 
+import './main.scss';
+
 @Component({
     selector: 'main-page',
     template: require('./main.html')
 })
 export class MainComponent implements OnInit {
+    lat: number = 53.9045;
+    lng: number = 27.5615;
+
     constructor(
         private router: Router,
         private accountService: AccountService
